e350c37c70f9682945c29ed1ea11763cca1f4992
Phillip <Phillip@yens-mbp>
2018-06-22 21:26:02 -0700

    finished obedient robot

--- src/modulo/ObedientRobot.java
+++ src/modulo/ObedientRobot.java
@@ -1,29 +1,37 @@
 package modulo;
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 public class ObedientRobot {
 public static void main(String[] args) {
-Robot rob = new Robot();{
-rob.	
-	
-	
-	
-	
-	
-	
-	
-}
 
-		
+	String answer=JOptionPane.showInputDialog("what shape would you like?");
+if (answer.equalsIgnoreCase("square")) {
+	drawSquare();
+}
+else if (answer.equalsIgnoreCase("triangle")) {
+	drawTriangle();
+}
+else if (answer.equalsIgnoreCase("circle")) {
+	drawCircle();
+}		
+String answer2 = JOptionPane.showInputDialog("what color would you like? Red or Blue");
+if (answer2.equalsIgnoreCase("blue")) {
+	Robot rob = new Robot();
 	
-	
-	
+	rob.setWindowColor(0,0,255);
+}
+else if (answer2.equalsIgnoreCase("red")) {
+Robot rob = new Robot();
+	rob.setWindowColor(255,0,0);
+} 
 	
 	
 	
 	
 	
 }
-public void drawSquare() {
+public static void drawSquare() {
 	Robot rob = new Robot();
 	rob.penDown();
 	rob.setSpeed(100);
@@ -38,7 +46,7 @@ public void drawSquare() {
 	rob.move(100);
 	rob.turn(90);
 	}
-public void drawTriangle() {
+public static void drawTriangle() {
 	 Robot rob = new Robot();
 	 rob.penDown();
 	 rob.setSpeed(100);
@@ -48,5 +56,31 @@ rob.move(100);
 rob.turn(135);
 rob.move(150);	
 }
+public static void drawCircle() {
+	Robot rob = new Robot();{
+		
+		rob.penDown();	
+		rob.setSpeed(100);
+			rob.move(100);
+			rob.turn(45);
+			rob.move(100);
+			rob.turn(45);
+			rob.move(100);
+			
+			rob.turn(45);
+			rob.move(100);
+			rob.turn(45);
+			rob.move(100);
+			rob.turn(45);
+			rob.move(100);
+			rob.turn(45);
+			rob.move(100);
+			rob.turn(45);
+			rob.move(100);
+			rob.turn(45);
+			
+		}
 }
+}
+
 
